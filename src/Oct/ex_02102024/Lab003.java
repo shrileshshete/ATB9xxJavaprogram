a072c19176bc6b96af1dfb9efb1e10e98db293ea
shrileshshete <shrileshshete1227@gmail.com>
2024-10-08 17:54:30 +0530

    My Files

---
+++ src/Oct/ex_02102024/Lab003.java
@@ -0,0 +1,7 @@
+package Oct.ex_02102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Main Method is a entry point for any java program");
+    }
+}
