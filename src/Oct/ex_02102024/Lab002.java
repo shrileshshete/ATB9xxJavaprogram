86fe720ff5a8bc174df2227183cff5d0a9d7f013
shrileshshete <shrileshshete1227@gmail.com>
2024-10-08 17:41:38 +0530

    My Files

---
+++ src/Oct/ex_02102024/Lab002.java
@@ -0,0 +1,13 @@
+package Oct.ex_02102024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("10x1=10");
+        System.out.println("10x2=20");
+        System.out.println("10x3=30");
+        System.out.println("10x4=40");
+        System.out.println("...");
+        System.out.println("10x10=100");
+
+    }
+}
