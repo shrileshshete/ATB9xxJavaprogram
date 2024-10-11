852587848e060655c7ec8bd87e689bc06a5febb1
shrileshshete <shrileshshete1227@gmail.com>
2024-10-11 16:32:52 +0530

    My Files

--- src/Oct/ex_010102024/Lab007.java
+++ src/Oct/ex_010102024/Lab007.java
@@ -1,4 +1,16 @@
 package Oct.ex_010102024;
 
 public class Lab007 {
+    public static void main(String[] args) {
+        System.out.println("Find all the types of the literals supported by the Java.");
+        System.out.println("Integer Literals,\n " +
+                        "Floating-Point Literals, \n " +
+                        "Character Literals,\n " +
+                        "String Literals,\n " +
+                        "Boolean Literals, \n " +
+                        "Null Literals,\n " +
+                        "Backslash Literals "
+        );
+
+    }
 }
---
+++ src/Oct/ex_010102024/Lab008.java
@@ -0,0 +1,14 @@
+package Oct.ex_010102024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        System.out.println("Some of the Examples of these literals are given below-");
+        System.out.println("1.Integer Literals: Includes decimal, octal, hexadecimal, and binary");
+        System.out.println("2.Floating-Point Literals: Includes double and float types");
+        System.out.println("3.String Literals: Contains text inside double quotes.");
+        System.out.println("4.Character Literal: Demonstrates regular characters and escape sequences \n" +
+                "(ex- newline\\n, horizontal tab\\t etc).");
+        System.out.println("5.Boolean Literals: Uses true and false.");
+        System.out.println("6.Null Literals:Contains Null value");
+    }
+}
