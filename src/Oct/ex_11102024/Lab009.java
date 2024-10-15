cbad141c02b6aaab22015d2344b59275051f453a
shrileshshete <shrileshshete1227@gmail.com>
2024-10-15 16:35:48 +0530

    My Files

--- src/Oct/ex_11102024/Lab009.java
+++ src/Oct/ex_11102024/Lab009.java
@@ -1,4 +1,13 @@
 package Oct.ex_11102024;
 
 public class Lab009 {
+    public static void main(String[] args) {
+        System.out.println("Types of operators in Java");
+        System.out.println("1.Arithmatic operators");
+        System.out.println("2.Relational Operators");
+        System.out.println("3.Logical Operators");
+        System.out.println("4.Bitwise Operators");
+        System.out.println("5.Unary operator");
+        System.out.println("5.Ternary Operators");
+    }
 }
---
+++ src/Oct/ex_11102024/Lab010.java
@@ -0,0 +1,14 @@
+package Oct.ex_11102024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+     System.out.println("Examples of operator");
+        System.out.println("Arithmetic Operators, \n "+
+                 "Addition, \n " +
+                 "Subtraction, \n " +
+                 "Multiplication,\n " +
+                 "Division, \n "+
+                 "Modulus");
+
+    }
+}
